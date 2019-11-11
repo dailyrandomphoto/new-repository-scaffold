0aebf8a2383dadf48359725a9f2869ee37fb779e
dailyrandomphoto <dailyrandomphoto@gmail.com>
2019-11-11 22:45:45 +0800

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const {expect} = require('chai');
+const lib = require('new-repository-scaffold');
+
+describe('something', () => {
+  it('should do something', () => {
+    expect(lib).to.be.a('object');
+  });
+});
